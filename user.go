256bcc56cd5d3e7f6315c73db3a5856ac1644a5e
Robert McLeod <robert@autogrow.com>
2018-03-08 23:17:32 +1300

    add nicer method to create user, find user

--- user.go
+++ user.go
@@ -6,16 +6,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+
+	"github.com/grafana/grafana/pkg/api/dtos"
 )
 
 // User represents a Grafana user
 type User struct {
-	Id      int64
-	Email   string
-	Name    string
-	Login   string
-	OrgId   string
-	IsAdmin bool
+	Id       int64  `json:"id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Login    string `json:"login"`
+	OrgId    string `json:"org_id"`
+	IsAdmin  bool   `json:"is_admin"`
+	Password string `json:"password,omitempty"`
+}
+
+func (u User) Using(c *Client, orgID int64) error {
+	req, err := c.newRequest("POST", fmt.Sprintf("/api/users/%d/using/%d", u.Id, orgID), nil)
+	if err != nil {
+		return err
+	}
+
+	req.URL.User = nil
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	return nil
 }
 
 // Users returns all the users from Grafana
@@ -43,17 +64,54 @@ func (c *Client) Users() ([]User, error) {
 	return users, err
 }
 
-func (c *Client) UpdateDefaultOrg(userID, orgID int64) error {
+func (c *Client) User(id int64) (User, error) {
+	user := User{}
+	req, err := c.newRequest("GET", fmt.Sprintf("/api/users/%d", id), nil)
+	if err != nil {
+		return user, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return user, err
+	}
+	if resp.StatusCode != 200 {
+		return user, errors.New(resp.Status)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return user, err
+	}
+	err = json.Unmarshal(data, &user)
+	if err != nil {
+		return user, err
+	}
+	user.Id = id
+	return user, err
+}
+
+// NewUser creates a new user by wrapping the CreateUserForm method to
+// avoiding requiring a dependency on Grafana code in your code
+func (c *Client) NewUser(u User) error {
+	form := dtos.AdminCreateUserForm{}
+	form.Password = u.Password
+	form.Email = u.Email
+	form.Name = u.Name
+	form.Login = u.Login
+
+	return c.CreateUserForm(form)
+}
+
+// SwitchUserOrg will switch the current organisation (uses basic auth)
+func (c *Client) SwitchUserOrg(userID, orgID int64) error {
 	req, err := c.newRequest("POST", fmt.Sprintf("/api/users/%d/using/%d", userID, orgID), nil)
 	if err != nil {
 		return err
 	}
 
 	req.URL.User = nil
-	log.Printf("%+v", req.URL)
-	req.Header.Set("Authorization", "Basic "+c.authBasic)
-	log.Printf("%+v", req.Header)
 	resp, err := c.Do(req)
+	data, _ := ioutil.ReadAll(resp.Body)
+	log.Println("hihiihi", string(data))
 	if err != nil {
 		return err
 	}
@@ -62,4 +120,29 @@ func (c *Client) UpdateDefaultOrg(userID, orgID int64) error {
 	}
 
 	return nil
+}
+
+// UserByEmail will find a user by their email address
+func (c *Client) UserByEmail(email string) (User, error) {
+	user := User{}
+	req, err := c.newRequest("GET", "/api/users/lookup?loginOrEmail="+email, nil)
+	if err != nil {
+		return user, err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return user, err
+	}
+	if resp.StatusCode != 200 {
+		return user, errors.New(resp.Status)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return user, err
+	}
+	err = json.Unmarshal(data, &user)
+	if err != nil {
+		return user, err
+	}
+	return user, err
 }
