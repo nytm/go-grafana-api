41b5fabceebb6b48c3a3acb5c0cb93cb63be499c
Malcolm Jones <malcolm.jones@nytimes.com>
2020-01-09 23:41:25 -0800

    add other datasource types

--- datasource.go
+++ datasource.go
@@ -32,18 +32,62 @@ type DataSource struct {
 
 // JSONData is a representation of the datasource `jsonData` property
 type JSONData struct {
-	AssumeRoleArn           string `json:"assumeRoleArn,omitempty"`
+	// Used by all datasources
+	TlsAuth           bool `json:"tlsAuth,omitempty"`
+	TlsAuthWithCACert bool `json:"tlsAuthWithCACert,omitempty"`
+	TlsSkipVerify     bool `json:"tlsSkipVerify,omitempty"`
+
+	// Used by Graphite
+	GraphiteVersion string `json:"graphiteVersion,omitempty"`
+
+	// Used by Prometheus, Elasticsearch, InfluxDB, MySQL, PostgreSQL and MSSQL
+	TimeInterval string `json:"timeInterval,omitempty"`
+
+	// Used by Elasticsearch
+	EsVersion       int64  `json:"esVersion,omitempty"`
+	TimeField       string `json:"timeField,omitempty"`
+	Interval        string `json:"inteval,omitempty"`
+	LogMessageField string `json:"logMessageField,omitempty"`
+	LogLevelField   string `json:"logLevelField,omitempty"`
+
+	// Used by Cloudwatch
 	AuthType                string `json:"authType,omitempty"`
-	CustomMetricsNamespaces string `json:"customMetricsNamespaces,omitempty"`
+	AssumeRoleArn           string `json:"assumeRoleArn,omitempty"`
 	DefaultRegion           string `json:"defaultRegion,omitempty"`
-	TlsSkipVerify           bool   `json:"tlsSkipVerify,omitempty"`
-	HttpMethod              string `json:"httpMethod,omitempty"`
-	QueryTimeout            string `json:"queryTimeout,omitempty"`
-	TimeInterval            string `json:"timeInterval,omitempty"`
+	CustomMetricsNamespaces string `json:"customMetricsNamespaces,omitempty"`
+
+	// Used by OpenTSDB
+	TsdbVersion    string `json:"tsdbVersion,omitempty"`
+	TsdbResolution string `json:"tsdbResolution,omitempty"`
+
+	// Used by MSSQL
+	Encrypt string `json:"encrypt,omitempty"`
+
+	// Used by PostgreSQL
+	Sslmode         string `json:"sslmode,omitempty"`
+	PostgresVersion int64  `json:"postgresVersion,omitempty"`
+	Timescaledb     bool   `json:"timescaledb,omitempty"`
+
+	// Used by MySQL, PostgreSQL and MSSQL
+	MaxOpenConns    int64 `json:"maxOpenConns,omitempty"`
+	MaxIdleConns    int64 `json:"maxIdleConns,omitempty"`
+	ConnMaxLifetime int64 `json:"connMaxLifetime,omitempty"`
+
+	// Used by Prometheus
+	HttpMethod   string `json:"httpMethod,omitempty"`
+	QueryTimeout string `json:"queryTimeout,omitempty"`
 }
 
 // SecureJSONData is a representation of the datasource `secureJsonData` property
 type SecureJSONData struct {
+	// Used by all datasources
+	TlsCACert         string `json:"tlsCACert,omitempty"`
+	TlsClientCert     string `json:"tlsClientCert,omitempty"`
+	TlsClientKey      string `json:"tlsClientKey,omitempty"`
+	Password          string `json:"password,omitempty"`
+	BasicAuthPassword string `json:"basicAuthPassword,omitempty"`
+
+	// Used by Cloudwatch
 	AccessKey string `json:"accessKey,omitempty"`
 	SecretKey string `json:"secretKey,omitempty"`
 }
